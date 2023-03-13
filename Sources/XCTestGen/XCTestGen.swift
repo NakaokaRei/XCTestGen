22c240b22acbca129bbc72c67cd21775109ad5bf
NakaokaRei <reideeplearning@gmail.com>
2023-03-13 21:42:44 +0900

    fix output to dir

--- Sources/XCTestGen/XCTestGen.swift
+++ Sources/XCTestGen/XCTestGen.swift
@@ -16,11 +16,19 @@ struct XCTestGen: AsyncParsableCommand {
     @Argument(help: "input file path")
     var input: String
 
-    @Argument(help: "output file path")
+    @Argument(help: "output dir path. do not include the trailing slash.")
     var output: String
 
     @Option(name: .shortAndLong, help: "Maximum tokens")
     var maxTokens: Int = 1000
+
+    func outputTestsPath() -> String {
+        let fileName = URL(fileURLWithPath: input).lastPathComponent
+        let fileNameWithoutExtension = (fileName as NSString).deletingPathExtension
+        let fileExtension = (fileName as NSString).pathExtension
+        let newFileName = fileNameWithoutExtension + "Tests." + fileExtension
+        return output + "/" + newFileName
+    }
 
     func generateXCTest() async throws {
         let order = "Please write a unit test (XCTest) of the following Swift code. No explanatory text is required as we would like to save your output as source code as is."
@@ -42,7 +50,7 @@ struct XCTestGen: AsyncParsableCommand {
         }
 
         do {
-            try FileManager.saveTextFile(text: testCode, atPath: output)
+            try FileManager.saveTextFile(text: testCode, atPath: outputTestsPath())
         } catch {
             throw XCTestGenError.saveFileError
         }
@@ -51,12 +59,12 @@ struct XCTestGen: AsyncParsableCommand {
     mutating func run() async throws {
         do {
             try await generateXCTest()
-            print("Generated to " + output)
+            print("Generated to " + outputTestsPath())
         } catch {
             if let xcTestError = error as? XCTestGenError {
                 print(xcTestError.description)
             } else {
-                print("Some Error")
+                print("Error")
             }
         }
     }
